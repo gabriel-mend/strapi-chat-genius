672035cd67c26b590cf3d6b292a13b624157c173
gabriel-mend <crowofcode@gmail.com>
2023-12-26 19:07:54 -0300

    feat: conectando com chatgpt

--- src/plugins/imagine/admin/src/pages/HomePage/index.tsx
+++ src/plugins/imagine/admin/src/pages/HomePage/index.tsx
@@ -6,13 +6,30 @@
 
 import React, { useState } from 'react';
 import pluginId from '../../pluginId';
-import { propmt } from '../../services/api';
+// import { propmt } from '../../services/api';
+import OpenAI from "openai";
 
 const HomePage = () => {
+
+
+
+  const openai = new OpenAI({
+    apiKey: '',
+    dangerouslyAllowBrowser: true
+  });
+
+  async function propmt(content: string) {
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "system", content }],
+      model: "gpt-3.5-turbo",
+    });
+
+    return completion.choices[0]
+  }
   const [text, setText] = useState('')
 
   async function handlePrintPrompt() {
-    const result = await propmt({ content: text })
+    const result = await propmt(text)
     console.log(result)
   }
 
