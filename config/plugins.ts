0ca1513ae26b0329b9a34ee12973165095bfdbca
gabriel-mend <crowofcode@gmail.com>
2023-12-28 17:11:43 -0300

    refactor: removendo primeiro plugin de exemplo

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,8 +1,4 @@
 export default {
-  'imagine': {
-    enabled: true,
-    resolve: './src/plugins/imagine'
-  },
   'strapi-with-chatgpt': {
     enabled: true,
     resolve: './src/plugins/strapi-with-chatgpt'
