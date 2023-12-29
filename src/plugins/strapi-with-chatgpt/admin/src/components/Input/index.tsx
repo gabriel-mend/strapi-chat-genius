ade671f3bad7c2e381ace8c87fcd4b56157f3b43
gabriel-mend <crowofcode@gmail.com>
2023-12-29 19:35:49 -0300

    feat: separando lógica de prompt do componente em um hook de prompt

--- src/plugins/strapi-with-chatgpt/admin/src/components/Input/index.tsx
+++ src/plugins/strapi-with-chatgpt/admin/src/components/Input/index.tsx
@@ -6,71 +6,28 @@
 
 import React from 'react';
 import { Loader, Flex, IconButton } from '@strapi/design-system';
-import OpenAI from 'openai';
+import { Write, Refresh, Collapse, File } from '@strapi/icons';
+
 import * as S from './styles';
-import { File } from '@strapi/icons';
-import SvgCollapse from '@strapi/icons/Collapse.js';
-import SvgRefresh from '@strapi/icons/Refresh.js';
-import { Write } from '@strapi/icons';
+
 import { useIntl } from 'react-intl';
 import getTrad from '../../utils/getTrad';
+import { usePrompt } from '../../hooks/usePrompt'
 
 
 const Input =  React.forwardRef(() => {
   const { formatMessage } = useIntl();
-  const [result, setResult] = React.useState('')
-  const [apiKey, setApiKey] = React.useState('')
-  const [content, setContent] = React.useState('')
-  const [isLoading, setIsLoading] = React.useState(false)
-  const [error, setError] = React.useState('')
+  const {
+    content,
+    result,
+    error,
+    handleCopyContent,
+    handlePromptContent,
+    isLoading,
+    handlePromptReduceContent,
+    setContent
+  } = usePrompt()
 
-  const openai = new OpenAI({
-    apiKey,
-    dangerouslyAllowBrowser: true
-  });
-
-  async function propmtContent(content: string) {
-    try {
-      const completion = await openai.chat.completions.create({
-        messages: [{ role: "system", content }],
-        model: "gpt-3.5-turbo",
-      });
-      setError('')
-      return completion.choices[0].message.content
-    } catch (e: any) {
-      setError(e.error.message as string)
-    }
-  }
-
-  async function handlePromptContent() {
-    setIsLoading(true)
-    const prompt = await propmtContent(content)
-    setIsLoading(false)
-    setResult(prompt as string)
-  }
-
-  async function handlePromptReduceContent() {
-    setIsLoading(true)
-    const reduceText = formatMessage({
-      id: getTrad("Component.Input.Function.Reduce"),
-      defaultMessage: ' Reduce this content with fewer lines.'
-    })
-    console.log(reduceText)
-    const prompt = await propmtContent(result.concat(reduceText as string))
-    setIsLoading(false)
-    setResult(prompt as string)
-  }
-
-  function handleCopyContent() {
-    navigator.clipboard.writeText(result)
-  }
-
-  React.useEffect(() => {
-    if(!apiKey) {
-      const local = localStorage.getItem('openai-key')
-      setApiKey(local as string);
-     }
-  }, [])
   return (
     <S.Container alignItems="flex-end" gap="3" direction="column">
       <S.Input
@@ -101,7 +58,7 @@ const Input =  React.forwardRef(() => {
               id: getTrad("Component.Input.IconButton.Reduce"),
               defaultMessage: 'Reduce content'
             })}
-            icon={<SvgCollapse />}
+            icon={<Collapse />}
             onClick={handlePromptReduceContent}
           />
           <IconButton
@@ -109,7 +66,7 @@ const Input =  React.forwardRef(() => {
               id: getTrad("Component.Input.IconButton.Reset"),
               defaultMessage: 'Regenerate content'
             })}
-            icon={<SvgRefresh />}
+            icon={<Refresh />}
             onClick={handlePromptContent}
           />
         </Flex>
---
+++ src/plugins/strapi-with-chatgpt/admin/src/hooks/usePrompt.ts
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import OpenAI from 'openai';
+import { useIntl } from 'react-intl';
+import getTrad from '../utils/getTrad';
+
+export function usePrompt () {
+  const { formatMessage } = useIntl();
+  const [result, setResult] = useState('')
+  const [apiKey, setApiKey] = useState('')
+  const [content, setContent] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState('')
+
+  const openai = new OpenAI({
+    apiKey,
+    dangerouslyAllowBrowser: true
+  });
+
+  async function propmtContent(content: string) {
+    try {
+      const completion = await openai.chat.completions.create({
+        messages: [{ role: "system", content }],
+        model: "gpt-3.5-turbo",
+      });
+      setError('')
+      return completion.choices[0].message.content
+    } catch (e: any) {
+      setError(e.error.message as string)
+    }
+  }
+
+  async function handlePromptContent() {
+    setIsLoading(true)
+    const prompt = await propmtContent(content)
+    setIsLoading(false)
+    setResult(prompt as string)
+  }
+
+  async function handlePromptReduceContent() {
+    setIsLoading(true)
+    const reduceText = formatMessage({
+      id: getTrad("Component.Input.Function.Reduce"),
+      defaultMessage: ' Reduce this content with fewer lines.'
+    })
+
+    const prompt = await propmtContent(result.concat(reduceText as string))
+    setIsLoading(false)
+    setResult(prompt as string)
+  }
+
+  function handleCopyContent() {
+    navigator.clipboard.writeText(result)
+  }
+
+  useEffect(() => {
+    if(!apiKey) {
+      const local = localStorage.getItem('openai-key')
+      setApiKey(local as string);
+     }
+  }, [])
+
+  return {
+    result,
+    content,
+    handleCopyContent,
+    handlePromptContent,
+    handlePromptReduceContent,
+    isLoading,
+    error,
+    setContent
+  }
+}
